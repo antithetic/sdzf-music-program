d60af42a00e30ff43d8f794dbdbeb3d7906983d7
Raúl Garcia <ego.war@icloud.com>
2026-08-30 00:51:35 -0700

    ✨ Sanity Media Browser
    Sanity media browser for better uploads and content control

--- packages/eslint.config.mjs
+++ packages/eslint.config.mjs
@@ -1,3 +1,10 @@
-import studio from "@sanity/eslint-config-studio";
+import studio from "@sanity/eslint-config-studio"
 
-export default [...studio];
+export default [
+  ...studio,
+  {
+    rules: {
+      semi: ["error", "never"],
+    },
+  },
+]
--- packages/sanity/src/index.ts
+++ packages/sanity/src/index.ts
@@ -2,15 +2,22 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schema";
+import { media } from "sanity-plugin-media";
 
-export const config = defineConfig({
-  name: "default",
-  title: "SDZF Music",
-
+export const sharedConfig = {
   projectId: "7mueck3w",
   dataset: "production",
+};
 
-  plugins: [structureTool(), visionTool()],
+export const sharedPlugins = [structureTool(), media(), visionTool()];
+
+export const config = defineConfig({
+  name: "sdzf-music",
+  title: "SDZF Music",
+
+  ...sharedConfig,
+
+  plugins: sharedPlugins,
 
   schema: {
     types: schemaTypes,
