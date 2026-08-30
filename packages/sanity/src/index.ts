034f0f4937e32a5771c8ae4429c5f96080b89598
Raúl Garcia <ego.war@icloud.com>
2026-08-30 02:49:09 -0700

    🩹 Isdev not working
    fix is dev

--- packages/sanity/src/index.ts
+++ packages/sanity/src/index.ts
@@ -1,4 +1,4 @@
-import { defineConfig, isDev } from "sanity";
+import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemaTypes";
@@ -9,11 +9,7 @@ export const sharedConfig = {
   dataset: "production",
 };
 
-export const sharedPlugins = [
-  structureTool(),
-  media(),
-  ...(isDev ? [visionTool()] : []),
-];
+export const sharedPlugins = [structureTool(), media(), visionTool()];
 
 export const config = defineConfig({
   name: "sdzf-music",
